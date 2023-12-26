4a465cbeb3aecc07c419f376ac5b157e4e54d2f5
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-26 17:48:44 +0800

    added canvas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("test")
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext("2d")
+
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const draw = () => {
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+draw()
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  draw()
+})
