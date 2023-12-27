85d57486228de4f29b158d0edaf836ba680d2c9f
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-27 23:11:56 +0800

    added sample particle movement

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const draw = () => {
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-draw()
+let x = 0, y = 0
 
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
-  draw()
 })
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const deltaX = Math.random() * 10 - 3; 
+  const deltaY = Math.random() * 10 - 3; 
+
+  x += deltaX;
+  y += deltaY;
+
+  x = Math.max(50, Math.min(canvas.width - 50, x));
+  y = Math.max(50, Math.min(canvas.height - 50, y));
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+  requestAnimationFrame(animate)
+}
+
+animate()
+
