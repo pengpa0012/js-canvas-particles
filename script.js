9b781e3733ddd602a1802cec7001dc83e427f5a7
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-28 20:17:10 +0800

    update createCircle

--- script.js
+++ script.js
@@ -17,36 +17,32 @@ window.addEventListener('resize', () => {
   canvas.height = window.innerHeight
 })
 
-function createCircle(x, y) {
+function createCircle(speed) {
+  if (x < nextPos.x) {
+    x += speed
+  }
+  if (x > nextPos.x){
+    x -= speed
+  }
+
+  if (y < nextPos.y) {
+    y += speed
+  }
+  if (y > nextPos.y){
+    y -= speed
+  }
   ctx.beginPath()
   ctx.arc(x, y, 10, 0, 2 * Math.PI)
   ctx.fill()
 }
 
 function animate() {
+  const posX = Math.random() * canvas.width
+  const posY = Math.random() * canvas.height
+  nextPos.x = posX
+  nextPos.y = posY
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  setInterval(() => {
-    const posX = Math.random() * canvas.width
-    const posY = Math.random() * canvas.height
-    nextPos.x = posX
-    nextPos.y = posY
-  }, 500)
-  
-  if (x < nextPos.x) {
-    x += 1
-  }
-  if (x > nextPos.x){
-    x -= 1
-  }
-
-  if (y < nextPos.y) {
-    y += 1
-  }
-  if (y > nextPos.y){
-    y -= 1
-  }
-  createCircle(x, y)
+  createCircle(5)
   requestAnimationFrame(animate)
 }
 
