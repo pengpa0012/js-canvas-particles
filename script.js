73d671d64f5e379677b3746eadce3843422d1004
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-31 19:42:19 +0800

    added color, drag particle and hide/show

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
 const circleUI = document.querySelector(".circle")
+const circleCoverUI = document.querySelector(".circle-cover")
 const speedUI = document.querySelector(".speed")
 const sizeUI = document.querySelector(".size")
 const colorUI = document.querySelector(".color")
@@ -13,7 +14,8 @@ canvas.height = window.innerHeight
 
 let paramsUI = {
   color: "#000000",
-  size: "10"
+  size: 12,
+  speed: 1
 }
 let particles = []
 
@@ -24,6 +26,7 @@ function initializeParticles() {
       y: Math.random() * canvas.height,
       speed: Math.random() * 2,
       size: Math.random() * 5 + 12,
+      color: "#000000"
     });
   }
 }
@@ -41,6 +44,7 @@ function createCircle(particle, i) {
 
   ctx.beginPath()
   ctx.arc(particle.x, particle.y, particle.size, 0, 2 * Math.PI)
+  ctx.fillStyle = particle.color
   ctx.fill()
 }
 
@@ -55,40 +59,60 @@ function animate() {
 window.addEventListener("DOMContentLoaded", () => {
   sizeUI.addEventListener("input", e => {
     paramsUI.size = e.target.value
-    editCircle(paramsUI.size,  paramsUI.color)
+    console.log(e.target.value)
+    editCircle(paramsUI)
+  })
+
+  speedUI.addEventListener("input", e => {
+    paramsUI.speed = e.target.value
   })
 
   colorUI.addEventListener("input", e => {
     paramsUI.color = e.target.value
-    editCircle(paramsUI.size, paramsUI.color)
+    editCircle(paramsUI)
   })
 
   dropdownBtn.addEventListener("click", () => {
+    dropdownBtn.textContent = dropdownBtn.textContent == "Show" ? "Hide" : "Show"
     coverUI.classList.toggle("toggle")
+    circleCoverUI.classList.toggle("toggle")
   })
 
-  function editCircle(size, color) {
-    circleUI.style.backgroundColor = color
-    circleUI.style.width = `${size}px`
-    circleUI.style.height = `${size}px`
-  }
-
-  function onDropCircle() {
+  circleUI.addEventListener("dragend", (e) => {
     // Push particles here with right parameters
+    const {x, y} = e
+    particles.push({
+      x,
+      y,
+      speed: paramsUI.speed,
+      size: paramsUI.size,
+      color: paramsUI.color
+    });
+  })
+
+  resetBtn.addEventListener("click", () => {
+    particles = []
+  })
+
+  function editCircle(circleParams) {
+    const {size, color} = circleParams
+    circleUI.style.backgroundColor = color
+    circleUI.style.width = `${parseInt(size) + 15}px`
+    circleUI.style.height = `${parseInt(size) + 15}px`
   }
 
   window.addEventListener('resize', () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
   })
-  editCircle(paramsUI.size, paramsUI.color)
+
+  editCircle(paramsUI)
   initializeParticles()
   animate()
 })
 
 
 // TO ADD
-// Add UI for adding particles
 // Add interaction on particles (combine, explode)
 // Add line on particles when near each other
 
